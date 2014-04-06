83ab5ad02df5b1b994b95fc2907efc9cc7654c27
Eric Cline <cline6@gmail.com>
2014-04-05 21:09:16 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# CEM_inc
+
+A sample webs
+
+
+## Installation
+
+TODO: Describe the installation process
+
+
+## Usage
+
+TODO: Write usage instructions
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/ericcline/src/CEM_inc/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
